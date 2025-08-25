a9fb527001c3f9f41ba9b89a60c9395aa01bb5e0
vkcosta <vito.knupp@gmail.com>
2025-08-25 17:06:05 +0100

    O componente tentivas é criado conforme a quantidade informada.

--- src/app/tentativas/tentativas.component.ts
+++ src/app/tentativas/tentativas.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, OnChanges } from '@angular/core';
+import { Component, OnInit, Input, OnChanges, SimpleChanges } from '@angular/core';
 import { Coracao } from '../shared/coracao.model';
 
 @Component({
@@ -11,23 +11,28 @@ export class TentativasComponent implements OnInit, OnChanges {
   @Input() public tentativas: number;
 
   public coracoes: Coracao[] = [
-    new Coracao(true),
-    new Coracao(true),
-    new Coracao(true),
+    // new Coracao(true),
+    // new Coracao(true),
+    // new Coracao(true),
   ]
 
   constructor() {
+  }
+
+  ngOnChanges(change: SimpleChanges) {
+
+    if (change.tentativas.currentValue) {
+      this.coracoes = Array.from({ length: this.tentativas }, () => new Coracao(true));
     }
 
-  ngOnChanges(){
-    if(this.tentativas !== this.coracoes.length){
+    if (this.coracoes.length > 0 && this.tentativas !== this.coracoes.length) {
       let indice = this.coracoes.length - this.tentativas;
-      this.coracoes[indice-1].cheio = false;
+      this.coracoes[indice - 1].cheio = false;
     }
-   
+
   }
 
   ngOnInit() {
-    
+
   }
 }
