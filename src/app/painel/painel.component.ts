a29837a4a6253184eef21bcc3086c498e42b7ba2
vkcosta <vito.knupp@gmail.com>
2025-08-26 17:20:57 +0100

    Alterando para 5 tentativas por padrão.

--- src/app/painel/painel.component.ts
+++ src/app/painel/painel.component.ts
@@ -17,7 +17,7 @@ export class PainelComponent implements OnInit, OnDestroy {
 
   public progresso: number = 0;
 
-  public tentativas: number = 3;
+  public tentativas: number = 5;
 
   @Output() public encerrarJogo: EventEmitter<string> = new EventEmitter();
 
